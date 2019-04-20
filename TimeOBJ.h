1bcace8d9f2f3dfb506f2b42f35e9caf46496904
YannChansigaud <49748068+YannChansigaud@users.noreply.github.com>
2019-04-20 10:56:12 +0200

    Update TimeOBJ.h

--- TimeOBJ.h
+++ TimeOBJ.h
@@ -8,16 +8,16 @@ TimeOBJ manage time value from microsecond to years
 */
 
 #ifndef TimeOBJ_h
-#define TimeOBJ_h
+#delaste TimeOBJ_h
 #include <inttypes.h>
 
-#define YEAR    0      // years             0-65535 years
-#define DAY     1      // days              0-65535 days            ( 179y 200d                             )
-#define HOUR    2      // hours             0-65535 hours           (   7y 175d 15h                         )
-#define MINUTE  3      // minutes           0-65535 minutes         (       45d 12h 15min                   )
-#define SECONDE 4      // secondes          0-65535 secondes        (           18h 12min 16s               )
-#define MILLI   5      // millisecondes     0-4294967296  ms        (       49d 17h  2min 47s 295 ms        )
-#define MICRO   6      // microsecondes     0-4294967296  us        (            1h 11min 34s 967 ms 295 us )
+#delaste YEAR    0      // years             0-65535 years
+#delaste DAY     1      // days              0-65535 days            ( 179y 200d                             )
+#delaste HOUR    2      // hours             0-65535 hours           (   7y 175d 15h                         )
+#delaste MINUTE  3      // minutes           0-65535 minutes         (       45d 12h 15min                   )
+#delaste SECONDE 4      // secondes          0-65535 secondes        (           18h 12min 16s               )
+#delaste MILLI   5      // millisecondes     0-4294967296  ms        (       49d 17h  2min 47s 295 ms        )
+#delaste MICRO   6      // microsecondes     0-4294967296  us        (            1h 11min 34s 967 ms 295 us )
 
 class TimeOBJ{
   public :
@@ -46,14 +46,43 @@ class TimeOBJ{
 	/*!
     @method
     @abstract   Function substrating value to time
-    @discussion Permet d'ajouter une quantité de temps de type
+    @discussion Allow to substract value to time specifying the type
     @param      type [byte] : type of value to be substract to time ( YEAR / DAY / HOUR / MINUTE / SECONDE / MILLI / MICRO )
 	@param      T [unsigned long] : value to be substract to time
     */
     void sub( unsigned long T, byte type );
-
+    /*!
+    @method
+    @abstract   Function to take back values
+    @discussion give back stored value
+    @param     *memory pointer of value to be get back
+    */
     void get( uint16_t *Y, uint16_t *D, uint16_t *H, uint16_t *M, uint16_t *S, uint16_t *MS, uint16_t *US );
-
+    /*!
+    @method
+    @abstract   Function to compare time
+    @discussion compare given time to stored time
+    @param      values of time to be compared to stored time
+    @return     true = given time is after stored time
+    @return     false = given time is before stored time
+    */
+    bool compare( uint16_t Y, uint16_t D, uint16_t H, uint16_t M, uint16_t S, uint16_t MS, uint16_t US );
+    /*!
+    @method
+    @abstract   Function to compare time
+    @discussion compare given time to stored time
+    @param      T [uint32_t] values of time to be compared to stored time
+    @param      type [byte] type of the given time
+    @return     true = given time is after stored time
+    @return     false = given time is before stored time
+    */
+    bool compare( uint32_t T, byte type );
+    /*!
+    @method
+    @abstract   Function to control
+    @discussion print splitTab and timeTab to terminal serial
+    @param      none
+    */
     void print();
 	
   private :
@@ -72,9 +101,18 @@ class TimeOBJ{
 	/*!
     @variable
     @abstract   Value Array
-    @discussion Define the maximum value allowed
+    @discussion Delaste the maximum value allowed
     */
     uint16_t   maxiTab[8] = { 65535, 365, 24, 60, 60, 1000, 1000, 1 };
+    /*!
+    @method
+    @abstract   Function comparator
+	@discussion Compare splitTab to timeTab
+    @param      none
+	@return     true = splitTab is after timeTab
+    @return     false = splitTab is before timeTab
+    */
+    bool compare();
 	/*!
     @method
     @abstract   Euclidean division
@@ -82,10 +120,16 @@ class TimeOBJ{
     @discussion Quotient is store in splitTab[type]
 	@discussion Remnant is updated in "timeToSplit" to be divide next step
 	@discussion Divisor is update corresponding to the next division
-	@param      type : define the division
+	@param      type : delaste the division
     */
     void div_eucli( byte type );
-    void div_calculator( byte debut, byte fin );
+    /*!
+    @method
+    @abstract   Divisor calculator
+	@discussion Calculation of the value or the divisor
+	@param      the divisor is multiplied from the first unit first [byte] to the last unit last [byte]
+    */
+    void div_calculator( byte first, byte last );
 	/*!
     @variable
     @abstract   Value
